f58437ec65037e889fc765b25c10ca9efdaff59d
Clinton <clinton.admpro29@gmail.com>
2024-10-23 14:00:19 -0300

    ...

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function sendEmail() {
 }
 function insta() {
     var click = window.document.querySelectorAll('insta')
+    window.open("https://instagram.com/wedsonconstrucoes")
 }
 function localize() {
     var click = window.document.querySelectorAll('localize')
